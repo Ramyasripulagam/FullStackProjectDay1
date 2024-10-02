4d2440e939706b82e231cd228b16d0dcc80665b4
Ramyasripulagam <ramyasripulagam5@gmail.com>
2024-10-02 20:29:00 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const rootElement=document.getElementById('root');
 const rootRef=ReactDOM.createRoot(rootElement);
-rootRef.render(<App/>);
+rootRef.render( 
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+  );
